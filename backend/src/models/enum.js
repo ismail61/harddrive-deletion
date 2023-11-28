2bc9eaf84df5a555cab186659de422b761c402f1
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2023-11-28 23:01:35 +0600

    Delete backend directory

--- backend/src/models/enum.js
+++
@@ -1,7 +0,0 @@
-const HardDriveDeletionStatusEnum = {
-    NON_STARTED: 'not started',
-    FAILED_DELETION: 'failed deletion',
-    DELETED: 'deleted'
-}
-
-module.exports = HardDriveDeletionStatusEnum;
--- backend/src/routes/customer.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const customerController = require('../controllers/customerController');
-
-router.post('/check-device', customerController.checkDevice);
-
-module.exports = router;
